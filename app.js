bbe7ee5ce88136072ebbf2301aa82486d2482827
Voyun <voyun0819@gmail.com>
2020-06-30 22:47:23 +0900

    0630

--- app.js
+++ app.js
@@ -4,17 +4,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { userRouter } from "./router"; 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on : http://localHost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from HOME");
 
 const handleProfile = (req, res) => res.send("You are on my Profile");
-
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -22,8 +17,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.get("/", handleHome); 
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter); 
+
+export default app; 
