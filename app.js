06f2957aaf6b30ad833017b2000e4d013fc9e43a
Voyun <voyun0819@gmail.com>
2020-07-01 16:26:30 +0900

    commit 0701

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import userRouter  from "./routers/userRouter"; 
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
 const app = express();
 
 app.use(cookieParser());
