d2b12c6612a2471de004bbbc15ba414bfbb33132
Voyun <voyun0819@gmail.com>
2020-07-07 21:31:20 +0900

    commit 0707

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
-
+/*
+    Pug과 express에는 view 파일들의 위치에 관한 기본 설정이 있음
+    만약 그 설정을 바꾸고 싶다면, 'views' 설정을 바꾸면 됨
+    application의 화면이 담긴 디렉토리나 디렉토리의 배열을 입력하면 됨
+    html 파일을 저장해야 하는 폴더의 기본 값은 프로젝트 작업 디렉토리+'/views'임
+    
+*/
+app.set("view engine","pug");  // view engine 설정값을 pug으로 바꿈
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ controllers/videoController.js
@@ -0,0 +1,10 @@
+// Global Router
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
+
+// Video Router
+export const videos = (req, res) => res.render("videos");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
