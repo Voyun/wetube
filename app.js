e78001ad54513d4c5bb103636cc5a48588c7170e
Voyun <voyun0819@gmail.com>
2020-09-10 19:55:39 +0900

    commit 0910

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
-import userRouter  from "./routers/userRouter"; 
+import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 
@@ -20,11 +20,12 @@ const app = express();
 
 */
 app.use(helmet());
-app.set("view engine","pug");  // view engine 설정값을 pug으로 바꿈
+app.set("view engine", "pug"); // view engine 설정값을 pug으로 바꿈
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 /* local 미들웨어 설정 방식 3가지
@@ -42,10 +43,10 @@ const localsMiddleware = (req, res, next) =>{
 
 }
 */
-app.use(localsMiddleware); // 위치 파악 중요 
+app.use(localsMiddleware); // 위치 파악 중요
 
-app.use(routes.home, globalRouter); // join login search home 이런것을 다룰 url 
+app.use(routes.home, globalRouter); // join login search home 이런것을 다룰 url
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
-export default app; 
+export default app;
