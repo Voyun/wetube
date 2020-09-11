afa3226e86d33cb83fe72a6dbe267cc593a033e6
Voyun <voyun0819@gmail.com>
2020-09-11 21:21:03 +0900

    commit 0911

--- middlewares.js
+++ middlewares.js
@@ -4,14 +4,14 @@ import routes from "./routes";
 export const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = "Wetube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id : 1
-    };
+  res.locals.siteName = "Wetube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
 
-    next();
+  next();
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
