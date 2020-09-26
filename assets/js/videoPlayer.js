3f7f2642da7728658cd7eef732bbd1c388b0c292
Voyun <voyun0819@gmail.com>
2020-09-26 23:07:44 +0900

    commit 0926

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -8,6 +8,13 @@ const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("jsVolume");
 //let videoPlayer;
 
+const registerView = () => {
+  const videoId = window.location.href.split("/videos/")[1];
+  fetch(`/api/${videoId}/view`, {
+    method: "POST",
+  });
+};
+
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
@@ -88,6 +95,7 @@ function setTotalTime() {
 }
 
 function handleEnded() {
+  registerView();
   videoPlayer.currentTime = 0;
   playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
@@ -112,6 +120,7 @@ function handleDrag(event) {
 }
 
 function init() {
+  //videoPlayer.currentTime = 630;
   videoPlayer.volume = 0.5;
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
